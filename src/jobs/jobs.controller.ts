62532b3421df5177b4bb5d570bad827c89c56ecf
Favizille <89010415+Favizille@users.noreply.github.com>
2022-10-11 03:23:58 +0100

    function to delete Job

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Patch, ParseIntPipe, Delete} from '@nestjs/common';
+import { JobDto } from './jobs.dto';
+import { JobsService } from './jobs.service';
 
 @Controller('jobs')
-export class JobsController {}
+export class JobsController {
+    constructor (private jobService: JobsService){}
+    
+    @Get()
+    getAll()
+    {
+        return this.jobService.getAll();
+    }
+
+    @Get('/:id')
+    async getJobById(@Param('id') id:number)
+    {
+        return this.jobService.getJobById(id);
+    }
+
+    @Post()
+    async createJob(@Param('id') id:number, @Body() jobDto:JobDto)
+    {
+        return await this.jobService.createJob(jobDto);
+    }
+
+    @Patch('/:id')
+    async updateJob(@Param('id') id:number, @Body() jobDto:JobDto) 
+    { 
+        return await this.jobService.updateJob(id, jobDto);
+    }
+
+    @Delete('/:id')
+    deleteJob(@Param('id', ParseIntPipe) id:number): Promise<string>
+    {
+        return this.jobService.deleteJob(id);
+    }
+}
---
+++ src/jobs/jobs.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty } from "class-validator"
+
+export class JobDto {
+    @IsNotEmpty()
+    title:string
+
+    @IsNotEmpty()
+    description:string
+
+    @IsNotEmpty()
+    nationality:string
+}
